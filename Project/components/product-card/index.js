71e69ef71df1145f9db5e70e9b0712a9c8203e4f
RedAlexDad <91482353+RedAlexDad@users.noreply.github.com>
2023-03-29 16:44:16 +0300

    Удаление всех уведомлений после нажатия на кнопку

--- Project/components/product-card/index.js
+++ Project/components/product-card/index.js
@@ -8,12 +8,13 @@ export class ProductCardComponent {
             `
             <div class="card" id="id-card-block-${data.id}" style="width: 300px;">
                 <img class="card-img-top" src="${data.src}" alt="картинка">
-                <div class="card-body">
+                <div class="card-body" id="id-card-body">
                     <h5 class="card-title">${data.title}</h5>
                     <p class="card-text">${data.text}</p>
                     <button class="btn btn-primary" id="click-card-${data.id}" data-id="${data.id}">Купить</button>
+                    <button class="btn btn-primary" id="click-delete-${data.id}" data-id="${data.id}">Очистить</button>
                 </div>
-                <div id="liveAlertPlaceholder-${data.id}"></div>
+                <div id="livealertplaceholder-${data.id}"></div>
                 <button type="button" class="btn btn-primary" id="liveAlertBtn-${data.id}">Погладить собачку</button>
             </div>
             `
@@ -30,16 +31,47 @@ export class ProductCardComponent {
         // console.log('project/components/product-card/index.js; addListeners, data.title:', data.title)
     }
 
+    deleteButton(data) {
+        try {
+            // debugger
+            // const alertPlaceholder = document.getElementById(`liveAlertPlaceholder-${data.id}`)
+
+            document.getElementById(`click-delete-${data.id}`)
+            .addEventListener("click", () => {
+                // var parent = document.getElementById(`click-delete-${data.id}`);
+                // var child = document.getElementById(`id-btn-close`);
+                // parent.removeChild(child);
+                const alertPlaceholder = document.getElementById(`id-card-block-${data.id}`)
+
+                document.getElementById(`livealertplaceholder-${data.id}`).outerHTML=`<div id= livealertplaceholder-${data.id}>`
+                // const wrapper = document.createElement(`livealertplaceholder-${data.id}`)
+
+                // wrapper.innerHTML = [   
+                //     `<div id= livealertplaceholder-{data.id}>`,
+                //     '</div>'
+                // ].join('')
+                // document.getElementById(`livealertplaceholder-${data.id}`).append(wrapper)
+                // alertPlaceholder.append(wrapper)
+                // alertPlaceholder.append(wrapper)
+                console.log('TEst')
+            })
+        }
+        catch{
+            console.log('Отсутствует класс alert alert-success alert-dismissible')
+        }
+    }
+
     addClickNotification(data) {
         // debugger
-        const alertPlaceholder = document.getElementById(`liveAlertPlaceholder-${data.id}`)
+        const alertPlaceholder = document.getElementById(`livealertplaceholder-${data.id}`)
         // console.log('1', alertPlaceholder)
         const alert = (message, type) => {
             // debugger
-            const wrapper = document.createElement(`id-card-block-${data.id}`)
+            const wrapper = document.createElement(`id-alert-${data.id}`)
+            
             // console.log('1 wrapper:', wrapper)
             wrapper.innerHTML = [
-                `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+                `<div class="alert alert-${type} alert-dismissible" role="alert" id="id-btn-close">`,
                 `   <div>${message}</div>`,
                 '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Закрыть"></button>',
                 '</div>'
@@ -56,8 +88,6 @@ export class ProductCardComponent {
                 alert('Успешно! Собака довольна!', 'success')
             })
         }
-
-
         // console.log('project/components/product-card/index.js; addListeners, data:', data)
         // console.log('project/components/product-card/index.js; addListeners, data.title:', data.title)
     }
@@ -67,6 +97,7 @@ export class ProductCardComponent {
         this.parent.insertAdjacentHTML('beforeend', html)
         this.addListeners(data, listener)
         this.addClickNotification(data)
+        this.deleteButton(data)
         // this.func_accordion(data, listener)
         // console.log('project/components/product-card/index.js; render, data:', data)
         // console.log('project/components/product-card/index.js; render, html:', html)
